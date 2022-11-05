a07859bb4ecc247c70409fb77b97e45d4a2b7741
Licornhizix <licornhizix@gmail.com>
2022-11-05 10:58:35 +0100

    modification .env random secret token

--- controllers/user.js
+++ controllers/user.js
@@ -2,13 +2,15 @@ const bcrypt = require('bcrypt');
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 
+
+
+
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
         .then(hash => {
             const user = new User({
                 email: req.body.email,
                 password: hash
-
             });
             user.save()
                 .then(() => res.status(201).json({
@@ -36,19 +38,29 @@ exports.login = (req, res, next) => {
                     error: 'Utilisateur non trouvé !'
                 });
             }
-            bcrypt.compare(req.body.password, user.password)
+            bcrypt.compare(req.body.password, user.password) //verification des MDP via bcrypt
                 .then(valid => {
                     if (!valid) {
                         return res.status(401).json({
                             error: 'Mot de passe incorrect !'
                         });
                     }
+                    /* res.status(200).json({
+                         userId: user._id,
+                         token: jwt.sign({
+                                 userId: user._id
+                             },
+                             'RANDOM_TOKEN_SECRET', {
+                                 expiresIn: '24h'
+                             }
+                         )
+                     });*/
                     res.status(200).json({
                         userId: user._id,
                         token: jwt.sign({
                                 userId: user._id
                             },
-                            'RANDOM_TOKEN_SECRET', {
+                            process.env.RANDOM_TOKEN_SECRET, {
                                 expiresIn: '24h'
                             }
                         )
@@ -62,3 +74,14 @@ exports.login = (req, res, next) => {
             error
         }));
 };
+
+/*let rand = function() {
+    return Math.random().toString(36).substr(2); // remove `0.`
+};
+
+let token = function() {
+    return rand() + rand(); // to make it longer
+};
+
+token(); // "bnh5yzdirjinqaorq0ox1tf383nb3xr
+*/
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,14 @@
 const jwt = require("jsonwebtoken");
 
+
 module.exports = function (req, res, next) {
     try {
-        const token = req.headers.authorization.split(" ")[1];
-        const decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET");
-        const userId = decodedToken.userId;
+        const token = req.headers.authorization.split(" ")[1]; //recupération du token
+        const decodedToken = jwt.verify(token, process.env.RANDOM_TOKEN_SECRET); 
+        //const decodedToken = jwt.verify(token, token());
+        const userId = decodedToken.userId; //recupération du userid
         req.auth = {
-            userId
+            userId //recupération de la valeur de l'userid pour la transmettre aux routes pour vérification
         };
         if (req.body.userId && req.body.userId !== userId) {
             throw "Invalid user ID";
@@ -15,7 +17,18 @@ module.exports = function (req, res, next) {
         }
     } catch {
         res.status(403).json({
-            error: new Error("403: unauthorized request.")
+            error: new Error("403: unauthorized request.") // en cas de problème d'authentification
         });
     }
 };
+
+/*let rand = function() {
+    return Math.random().toString(36).substr(2); // remove `0.`
+};
+
+let token = function() {
+    return rand() + rand(); // to make it longer
+};
+
+token(); // "bnh5yzdirjinqaorq0ox1tf383nb3xr
+*/
